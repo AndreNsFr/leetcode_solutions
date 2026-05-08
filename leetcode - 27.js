1f9b95b5de5da91add5e89d68a03ef32e613fb2c
André do Nascimento Ferreira <72326296+AndreNsFr@users.noreply.github.com>
2026-05-08 18:32:38 -0300

    Add removeElement function to filter array values
    Implement function to remove specified value from array.

---
+++ leetcode - 27.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let count = nums.length;
+    let j = count - 1;
+    for(let e = 0; e < nums.length/2 ; e++){
+        
+        if(nums[e] == val){
+            nums[e] = undefined
+            count--;
+        }
+        if(nums[j] == val){
+            nums[j] = undefined
+            count--;
+        }
+        j--;
+    }
+
+    nums.sort()
+    
+    return count;
+};
