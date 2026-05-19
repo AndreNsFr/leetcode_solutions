98d49272549629995218e441341b7b0da20257df
AndreNsFr <andredonascimentoferreira309@gmail.com>
2026-05-18 22:24:32 -0300

    2723 add two promises solution

---
+++ solutions/Easy/leetcode - 2723 Add Two Promises.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    
+    let x1 = await promise1;
+    let x2 = await promise2;
+
+    return x1 + x2;
+    
+
+};
+
+/**
+ addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+   .then(console.log); // 4
+//  */
